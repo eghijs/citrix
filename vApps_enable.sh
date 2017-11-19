adee7a17d4c3164dc866e1c2e010b0b8446688e0
eghijs <eghijs@gmail.com>
2017-11-19 17:55:54 -0300

    Create vApps_enable.sh

---
+++ vApps_enable.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# 
+# Criado por: Erik P. GHijs
+# Data: 19/11/2017
+# Descricao: Este script tem a finalidade de ativar o auto-start das vm´s no Xenserver 7.2.
+#
+# Parametros de configuração
+cd /etc/init.d
+touch Start_vApps
+chmod a+x Start_vApps
+
+cat <<EOF > Start_vApps
+# AutoStart XenServer vApps with the tag autostart in their description
+# Script originally created by Raido Consultants - http://www.raido.be
+# Script updated and shared by E.Y. Barthel - http://www.virtues.it
+TAG="autostart"
+ 
+# helper function
+function xe_param()
+{
+    PARAM=$1
+    while read DATA; do
+        LINE=$(echo $DATA | egrep "$PARAM")
+        if [ $? -eq 0 ]; then
+            echo "$LINE" | awk 'BEGIN{FS=": "}{print $2}'
+        fi
+    done
+} # Get all Applicances
+sleep 20
+VAPPS=$(xe appliance-list | xe_param uuid)
+for VAPP in $VAPPS
+do
+    # debug info
+    # echo "Esther's AutoStart : Checking vApp $VAPP"
+    VAPP_TAGS="$(xe appliance-param-get uuid=$VAPP param-name=name-description)"
+    if [[ $VAPP_TAGS == *$TAG* ]]; then
+        # action info:
+        echo "starting vApp $VAPP";
+        xe appliance-start uuid=$VAPP;
+        sleep 20
+    fi
+done
+EOF
+ln -s ../init.d/script_vApps S99vApps-xen
